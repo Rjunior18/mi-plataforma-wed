85e89a4a9519d94ed7a5d4f46767591024efcaf3
Gamez <robert.gamez.quinonez@utelvt.edu.ec>
2022-09-08 14:18:07 -0500

    persona

---
+++ datos.sql
@@ -0,0 +1,4 @@
+create database  mi-plataforma-wed;
+use mi-plataforma-wed;
+
+create table persona(idpersona int,nombres varchar(20),apellidos varchar(20),cedula varchar(10),idgenero int,fechadenacimiento date,idnacionalidad int,idetnia int,idestadocivil int);
